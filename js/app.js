ce134b1f3be7344fc9d15e9ce95bcaec3fd4025d
nassir <nassirmz@gmail.com>
2016-03-21 23:35:53 -0700

    Add sticky Nav bar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,50 @@
 $(document).ready(function() {
   smoothScroll();
+  stickyNav();
 });
 
+//smooth scrolling 
 function smoothScroll() {
   $("nav a").click(function(event) {
     event.preventDefault();
     var sectionId = event.currentTarget.id + "Section";
     $("html body").animate({
+
       scrollTop: $("#" + sectionId).offset().top
     }, 1500);
   });
+}
+
+//sticky nav bar on scroll
+function stickyNav() {
+  var nav = $("nav ul");
+  var pos = nav.position();
+  var secondPageHeight = $("#portfolioSection").offset().top;
+  $(window).scroll(function() {
+    var windowPos = $(window).scrollTop();
+    if(windowPos >=secondPageHeight) {
+      nav.addClass("fixedTop");
+    } else {
+      nav.removeClass("fixedTop");
+    }
+  })
+}
+
+//animation on scroll
+
+function animateOnScroll() {
+  var windowPos = $(window).scrollTop();
+  $('.animate:not(.animated)').each(function () {
+    var $this = $this,
+    offsetTop = $this.offset().top;
+    if(windowPos >offsetTop) {
+      if($this.data("timeout")) {
+        window.setTimeout(function() {
+          $this.addClass("animated " + $this.data("animation"));
+        }, parseInt($this.data("timeout"), 10));
+      } else {
+        $this.addClass("animated " + $this.data("animation"));
+      }
+    }
+  })
 }
